9e953e59b78c5f2b51ef9e98b4c71b4cb4a4e779
19039518 <gurpreetgillfzr0029@gmail.com>
2020-02-05 14:45:15 -0500

    lab4 is done

---
+++ app/src/main/java/com/soogreyhounds/soogreyhoundsmobile/Photo.java
@@ -0,0 +1,54 @@
+package com.soogreyhounds.soogreyhoundsmobile;
+
+public class Photo {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    int id;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    String mTitle;
+
+    public String getUUID() {
+        return uuid;
+    }
+
+    public void setUUID(String uuid) {
+        this.uuid = uuid;
+    }
+
+    String uuid;
+
+    public String getURL() {
+        return url;
+    }
+
+    public void setURL(String url) {
+        this.url = url;
+    }
+
+    String url;
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    String note;
+
+}
